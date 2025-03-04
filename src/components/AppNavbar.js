080274f4594c1ecf40aef186d94be77b7962fe38
argie.orn <argie.orn@gmail.com>
2025-03-04 20:54:00 +0800

    Add activity code

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaRunning, FaDumbbell, FaTrophy, FaHeartbeat } from 'react-icons/fa';
+import '../App.css'; 
+
+export default function AppNavbar() {
+    return (
+        <Navbar expand="lg" className="app-navbar">
+            <Container>
+                <Navbar.Brand as={Link} to="/" className="navbar-brand">
+                    FitneZuitt <FaDumbbell />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/" className="nav-link">
+                            <FaTrophy /> Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/workouts" className="nav-link">
+                            <FaRunning />Workouts
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/nutrition" className="nav-link">
+                            <FaHeartbeat /> Nutrition
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/about" className="nav-link">
+                            About
+                        </Nav.Link>
+                        <Button as={Link} to="/login" className="nav-button">
+                            Login
+                        </Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
